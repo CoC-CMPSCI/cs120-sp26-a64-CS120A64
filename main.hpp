2bc7dc05e528246b80cf96bc6c4ecc8d1af67c05
Kyu Lee <kwooler@gmail.com>
2026-04-22 15:20:22 -0700

    Remove pre-filled solutions from main.hpp — restore blank TODO template
    Students should implement swapValues() themselves. Previously the three
    overloads contained working solutions which defeated the exercise.

--- main.hpp
+++ main.hpp
@@ -8,25 +8,14 @@ void swapValues(int &, int &, int &, int &);
 void swapValues(int &n1, int &n2)
 {
     // TODO
-    swap(n1, n2);
-    // END TODO
 }
 
 void swapValues(int &n1, int &n2, int &n3)
 {
     // TODO
-    int tmp;
-    tmp = n1;
-    n1 = n2;
-    n2 = n3;
-    n3 = tmp;
-    // END TODO
 }
 
 void swapValues(int &n1, int &n2, int &n3, int &n4)
 {
     // TODO
-    swap(n1, n4);
-    swap(n2, n3);
-    // END TODO
 }
